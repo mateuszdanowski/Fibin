2eb41f533cd51ae9747b00b1adc4cd6b499237b5
Mateusz Danowski <mateuszdanowski0@gmail.com>
2019-11-30 15:18:09 +0100

    add cmake

---
+++ cmake-build-debug/CMakeFiles/Fibin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Fibin.dir/fibin_example.cc.o"
+  "Fibin"
+  "Fibin.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Fibin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
