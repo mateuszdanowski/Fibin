728c1b49e598b45d16ec357fd807b7fa6e555973
Mateusz Danowski <mateuszdanowski0@gmail.com>
2019-11-30 15:15:16 +0100

    add fibin.h

---
+++ fibin.h
@@ -0,0 +1,81 @@
+#pragma once
+
+#include <type_traits>
+#include <typeinfo>
+#include <cstdio>
+
+// A type for Fibonacci numbers:
+template <unsigned int n>
+struct Fib
+{
+    static constexpr unsigned int value = Fib<n-1>::value + Fib<n-1>::value;
+};
+
+template<>
+struct Fib<0>
+{
+    static const unsigned int value = 1;
+};
+
+template<>
+struct Fib<1>
+{
+    static const unsigned int value = 1;
+};
+
+
+// Booleans:
+struct True {};
+struct False {};
+
+
+template <typename Condition, typename Then, typename Else>
+struct If {};
+
+template <typename T>
+struct Lit {};
+
+template <typename Expr>
+struct Eval {};
+
+// Literals evaluate to themselves:
+template <typename ValueType>
+struct Eval <Lit<ValueType>>
+{
+    typedef ValueType result;
+};
+
+
+// If true:
+template <typename Then, typename Else>
+struct Eval<If<True, Then, Else>> {
+    typedef typename Eval<Then>::result result;
+};
+
+// If false:
+template <typename Then, typename Else>
+struct Eval<If<False, Then, Else>> {
+    typedef typename Eval<Else>::result result;
+};
+
+// Evaluate the condition:
+template <typename Condition, typename Then, typename Else>
+struct Eval<If<Condition, Then, Else>> {
+    typedef typename Eval<
+            If<typename Eval<Condition>::result, Then, Else>
+            >::result
+    result;
+};
+
+
+// Fibin class:
+template <typename ValueType>
+struct Fibin {
+
+    // Works for all ValueType (shouldn't for const char* etc)
+    template <typename T>
+    constexpr static ValueType eval() {
+        return Eval<T>::result::value;
+    }
+};
+
